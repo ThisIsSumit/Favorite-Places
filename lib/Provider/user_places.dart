8cb26cd1e32606dd84b271ea728b962e5fc89918
ThisIsSumit <sumitkumar453827@gmail.com>
2024-07-07 17:06:50 +0530

    storing data localy

--- lib/Provider/user_places.dart
+++ lib/Provider/user_places.dart
@@ -1,12 +1,28 @@
 import 'dart:io';
-
+import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:favorite_places_app/model/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
-  void addPlace(String name, File image) {
-    final newPlace = Place(name: name, image: image);
+  void addPlace(String name, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+    final newPlace = Place(name: name, image: image, location: location);
+    final dbpath = await sql.getDatabasesPath();
+    sql.openDatabase(
+      path.join(dbpath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+      },
+      version: 1
+    );
+
     state = [newPlace, ...state];
   }
 }
--- lib/screens/add_new_places_screen.dart
+++ lib/screens/add_new_places_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:favorite_places_app/Provider/user_places.dart';
+import 'package:favorite_places_app/model/place.dart';
 import 'package:favorite_places_app/widgets/image_input.dart';
 import 'package:favorite_places_app/widgets/location_input.dart';
 
@@ -18,13 +19,17 @@ class AddNewPlacesScreen extends ConsumerStatefulWidget {
 class _AddNewPlacesScreenState extends ConsumerState<AddNewPlacesScreen> {
   String? _enteredTitle;
   File? _selectedImage;
+  //adding the lag and longitute of hamirpur HP because we dont have accest to google maps
+  
+  PlaceLocation selectedLocaion =
+      PlaceLocation(latitude: 31.6862, longitude: 76.5213, address: 'home');
   GlobalKey<FormState> globalKey = GlobalKey<FormState>();
   void addNewPlace() {
     if (globalKey.currentState!.validate() || _selectedImage != null) {
       globalKey.currentState!.save();
       ref
           .read(userPlacesProvider.notifier)
-          .addPlace(_enteredTitle!, _selectedImage!);
+          .addPlace(_enteredTitle!, _selectedImage!, selectedLocaion!);
 
       Navigator.of(context).pop();
     }
@@ -46,51 +51,52 @@ class _AddNewPlacesScreenState extends ConsumerState<AddNewPlacesScreen> {
         key: globalKey,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TextFormField(
-                autocorrect: true,
-                style: const TextStyle(color: Colors.white),
-                textAlign: TextAlign.center,
-                cursorColor: Colors.white,
-                onChanged: (value) => _enteredTitle = value,
-                decoration: const InputDecoration(
-                  label: Text("Title"),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextFormField(
+                  autocorrect: true,
+                  style: const TextStyle(color: Colors.white),
+                  cursorColor: Colors.white,
+                  onChanged: (value) => _enteredTitle = value,
+                  decoration: const InputDecoration(
+                    label: Text("Title"),
+                  ),
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length == 1 ||
+                        value.trim().length > 50) {
+                      return "Must be between 1 to 50";
+                    }
+                    return null;
+                  },
                 ),
-                validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      value.trim().length == 1 ||
-                      value.trim().length > 50) {
-                    return "Must be between 1 to 50";
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ImageInput(
-                onPickImage: (image) => _selectedImage = image,
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              LocationInput(),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                    onPressed: addNewPlace,
-                    child: const Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [Icon(Icons.add), Text("Add Place")],
-                    )),
-              )
-            ],
+                const SizedBox(
+                  height: 20,
+                ),
+                ImageInput(
+                  onPickImage: (image) => _selectedImage = image,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                LocationInput(),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                      onPressed: addNewPlace,
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [Icon(Icons.add), Text("Add Place")],
+                      )),
+                )
+              ],
+            ),
           ),
         ),
       ),
